7f90f33b85db0ac93fb7c1d8c06e0af8ccc5e7a3
lmendiboure <leo.mendiboure11@gmail.com>
2019-02-22 19:12:10 +0100

    average latency improvement

--- utils/subscriber.py
+++ utils/subscriber.py
@@ -26,20 +26,29 @@ def listen_socket(self,threadName, udp_port,ID):
                      socket.SOCK_DGRAM) # UDP
     sock.bind((UDP_IP, UDP_PORT))
     sock.setblocking(0)
-
-    previous_pkt_nb =0
-    average_latency = 0	    
+   
+    previous_pkt_nb =0 
+    average_latency = 0	 
+    init_time=0
+    end_time=0  
 
     while not self._stopevent.isSet(  ): # Quit the program when requested by the user
 	try:
 	        send_tmstp, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
 		cur_time = int(round(time.time() * config.timestamp_precision))
-        	print "\n%s, received message: %s" % (threadName,send_tmstp)
-		send_tmstp = int(send_tmstp)
-		
-		print  cur_time - send_tmstp
-		average_latency = (previous_pkt_nb*average_latency + cur_time - send_tmstp)/(previous_pkt_nb+1)
-		previous_pkt_nb += 1 
+		if (previous_pkt_nb ==0 and config.mode=="p"):
+				init_time=cur_time
+		if (config.mode=="p"):		
+        		print "\n%s, received message: %s" % (threadName,send_tmstp)
+			send_tmstp = int(send_tmstp)		
+			print  cur_time - send_tmstp
+			average_latency = (previous_pkt_nb*average_latency + cur_time - send_tmstp)/(previous_pkt_nb+1)
+			previous_pkt_nb += 1 
+			print previous_pkt_nb
+			if previous_pkt_nb==config.eval_packet_nb:
+				end_time=int(round(time.time() * config.timestamp_precision))
+		if (config.mode=="n"):
+			print "\n%s, received message: %s" % (threadName,send_tmstp)
 
 	except socket.error, e:
 		if e.args[0] == errno.EWOULDBLOCK:  # Non blocking socket
@@ -47,11 +56,12 @@ def listen_socket(self,threadName, udp_port,ID):
 		else:
 			print e
 			break
-
-    file_name= "data/clients/"+ID	
-    with open(file_name, "a") as myfile:
-	string = str(udp_port)+",latency: "+str(average_latency)+"\n"
-    	myfile.write(string)
+    if config.mode=="p":
+	print end_time, init_time
+    	file_name= "data/clients/"+ID	
+    	with open(file_name, "a+") as myfile:
+		string = str(udp_port)+",latency: "+str(average_latency)+"\n"
+    		myfile.write(string)
 
 
 def publish_message(IP,udp_port,message):
